4722879eba338f7e2f42b7b05f4c79b0daf50d10
Zacherybignall <Zacherybignall@gmail.com>
2020-01-29 16:32:53 -0600

    updated  server
    Server now takes in an array (2-d{i.e. a table}) from socket , sets up parse to read a string array into a array and stringify the take a array and Tostring it .
    Lsu now gets avg. of the whole tabel , goes cell by cell.

--- backend/LSU.js
+++ backend/LSU.js
@@ -1,3 +1,4 @@
+//ZB.
 var data = 0;
 exports.data = data; 
 exports.set = function(value) {
@@ -8,5 +9,24 @@ exports.get = function() {
     return data;
 };
 exports.runmodel = function(table_data) {
-    return table_data *2 ;
+    //getting avg from a 2d table(array/list)
+    var avg = 0;
+    var total  = 0;
+    var i,j,n = 0;
+    
+    console.log("Y length " + table_data.length);
+    console.log("X length " + table_data[0].length);
+    //console.log(table_data[0][0]);
+    //console.log(table_data[0]);
+    
+    for (i = 0;i!=table_data.length; i++){
+        for (j = 0;j != table_data[i].length; j++){
+            
+            total += table_data[i][j];
+            // note: table_data[j] returns a string not a int 
+            n++;
+        }
+    }
+    avg = (total/(n));
+    return avg;
 };
--- backend/web_server.js
+++ backend/web_server.js
@@ -1,18 +1,44 @@
-const WebSocket = require('ws');
-var myModle = require('./vader.js');
-const wss = new WebSocket.Server({ port: 8080 });
 
+const WebSocket = require('ws');
+var myModle = require('./LSU.js');
+const wss = new WebSocket.Server({ port: 8080 });
+console.log(`sever is wanting for connection`);
 wss.on('connection', (ws) => {
   ws.on('message', (message) => {
-    console.log(`Received message => ${message}`)
-    model_data = returnModelStats(parseFloat(message));
-    ws.send(model_data.toString());
+    //send 
+    var s;
+    console.log(`Received message => ${message}`);
+    //parse and stringify makes it to where this can send data across the socket
+    model_data = returnModelStats(message);
+    s =  JSON.stringify(model_data);
+    ws.send(s);
+    console.log("was sent back: " + s);
   });
 });
-function returnModelStats(data){
-  if (typeof data == 'number'){
-      return myModle.runmodel(data);
+//ZB.
+function returnModelStats(r){
+  //r =  recived
+  //checks is data is a array
+  if (Array.isArray(r)){
+    //parse  makes it to where this can read data from the socket
+    data = JSON.parse(r);
+    //pushes the data to the Model
+    return myModle.runmodel(data);
   } else{
-      return ("please but a numbers or table of numbers");
+      return ("please put a numbers or table of numbers in the form of an array ");
   }
 };
+//testing ZAB
+/*
+var data=[];
+var temp =[];
+var i,j;
+for (i = 0;i!=6; i++){
+  for (j = 0;j!=10; j++){
+      temp[j] = Math.round(Math.random() * 10);
+  }
+  data[i] = temp;
+}
+console.log(data);
+console.log(returnModelStats(data));
+*/
