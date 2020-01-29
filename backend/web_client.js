171c6a3be7b6edcac09da8f89208ebe63ec8693b
Coolmt3 <38844686+Coolmt3@users.noreply.github.com>
2020-01-29 16:24:16 -0600

    backend_clientside
    Client side creates and sends an array to the server. Receives an average back.

--- backend/web_client.js
+++ backend/web_client.js
@@ -2,7 +2,7 @@ const WebSocket = require('ws');
 const fs = require('fs');
 function runnewmodel(table){
     var DFM;
-    const url = 'ws://192.168.1.13:8080';
+    const url = 'ws://138.47.138.96:8080';
     const connection = new WebSocket(url);
     connection.onopen = () => {
     connection.send(table);
@@ -13,14 +13,29 @@ function runnewmodel(table){
     };
 
     connection.onmessage = (e) => {
-        console.log("indise the loop " + e.data)
+        console.log("Inside the loop, " + e.data)
         connection.close();
         fs.writeFile('modeldata.txt', e.data, (err) => {
             if (err) throw err;
-            console.log("data modeldata svaed");
+            console.log("data modeldata saved");
         });
         return DFM;
     };
 };
-var dataFM = runnewmodel(40);
+/*
+//Creates arrays within an array and calls it data.
+var data=[];
+var temp =[];
+var i,j,d;
+for (i = 0;i!=100; i++){    //Create 100 arrays
+  for (j = 0;j!=1000; j++){ //with 1000 numbers each.
+      temp[j] = Math.round(Math.random() * 10);
+  }
+  data[i] = temp;
+}
+
+//Turns the data array into a string to be parsed by the server.
+d = 'JSON.stringify(data);';
+*/
+var dataFM = runnewmodel(d);
 
