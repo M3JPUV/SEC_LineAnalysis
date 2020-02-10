f1865d3447f2febefd3d47141e3ac796c6c04eb3
Lane-A <lsa010@latech.edu>
2020-02-10 14:18:00 -0600

    Added sign up with commenrts

--- Express/index.js
+++ Express/index.js
@@ -7,12 +7,17 @@ const logger = require('./middleware/logger');
 //L. For handling templates
 const EH = require('express-handlebars')
 //L. anything we do is called ''app'' that is ran in the express framework
+const bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 const app = express();
 
 //L. init middleware
 app.use(logger);
 
 //For Handlebars
+
 
 //L. BodyParser (Used for json files)
 app.use(express.json());
@@ -26,11 +31,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 //L. placeholder
 app.use('/api/members', require('./routes/api/members'));
 
+//L. Sign up
+app.use('/api/signup', require('./routes/api/signup'));
+
 //L. all testing APIs go under this file name
 app.use('/api/tests', require('./routes/api/tests'));
 
 //L. Login API
 app.use('/api/login', require('./routes/api/login'));
+
 //L. Stores our port#
 const PORT = process.env.PORT || 5000;
 //L. Starts app and listens on the specified port number
